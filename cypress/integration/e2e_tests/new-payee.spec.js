2c7545fe50c1d777d135c698d5f9b278847af7f9
Fathur Rohim <trustoim@gmail.com>
2020-04-06 14:58:25 +0700

    payee test

---
+++ cypress/integration/e2e_tests/new-payee.spec.js
@@ -0,0 +1,31 @@
+
+describe('New Payee Test', function() {
+    before(() => {
+        cy.visit("http://zero.webappsecurity.com/index.html")
+        cy.get('#signin_button').click()
+
+        cy.fixture('user').then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').type(password)
+            cy.get('#user_remember_me').click()
+            cy.contains('Sign in').click()
+        })
+    });
+    
+    it('Should add new payee to the list', () => {
+        cy.get('#pay_bills_tab').click()
+        cy.contains('Add New Payee').click()
+        cy.get('#np_new_payee_name').type('Name').should('have.value', 'Name')
+        cy.get('#np_new_payee_address').type('Address')
+        cy.get('#np_new_payee_account').type('3456545678').should('have.value', '3456545678')
+        cy.get('#np_new_payee_details').type('Detail').should('have.value', 'Detail')
+        cy.get('#add_new_payee').click()
+    });
+
+    it('Should show success message', () => {
+        cy.get('#alert_content').should('be.visible').and('contain', 'The new payee Name was successfully created.')
+    });
+});
