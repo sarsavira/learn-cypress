38bfbdfc2ff8c76daf480fea6d1c4868eb09c4cc
Fathur Rohim <trustoim@gmail.com>
2020-04-11 14:04:59 +0700

    single element test

--- cy-visual-tests/cypress/integration/image.spec.js
+++ cy-visual-tests/cypress/integration/image.spec.js
@@ -6,11 +6,21 @@ describe('Visual Regression', function() {
     sizes.forEach(size => {
         pages.forEach(page => {
             it(`Should match ${page} in resolution ${size}`, () => {
+                let currentTime = new Date(Date.UTC(2020, 4, 11)).getDate()
+                cy.clock(currentTime)
                 cy.setResolution(size)
                 cy.visit(page)
                 cy.matchImageSnapshot()
             });
         })
     })
+});
+    
+
+describe('Single Element Snapshot', function() {
+    it('Should match a single element on the page', () => {
+        cy.visit('http://example.com/')
+        cy.get('h1').matchImageSnapshot()
+    });
 });
     
