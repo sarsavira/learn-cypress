8f61467d3e0dc85c51e5d27774ac5b4a70386c29
Fathur Rohim <trustoim@gmail.com>
2020-04-24 10:51:15 +0700

    create bdd test with cucumber

---
+++ cy-bdd-tests/cypress/plugins/index.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
